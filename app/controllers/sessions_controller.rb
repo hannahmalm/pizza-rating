1415b70786503c71281d459831bcf8150058dfca
Hannah Malm <hannahemalm616@gmail.com>
2021-10-13 18:10:07 -0400

    finalized login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -17,18 +17,14 @@ class SessionsController < ApplicationController
     def create 
         @user = User.find_by(username: params[:user][:username])
             #try is an active support method, calling try on the user, if the user is not nil, then they are authenticating the params password against what the user put in
-            #before calling the method, it trys to see if the user is nil
+            #before calling the method, it trys to see if the user is nil. If user is not nil, it will authenticate
         if @user.try(:authenticate, params[:user][:password])
-            session[:user_id] = @user.id 
-            redirect_to user_path(@user)
         # if @user && @user.authenticate(password: params[:user][:password])
-        #     session[:user_id] = @user.id 
-        #     #user_path(@user) is the same as user_path(user_id)
-        #     redirect_to user_path(@user)
+            session[:user_id] = @user.id #set the session 
+            redirect_to user_path(@user) #user_path(@user) is the same as user_path(user_id)
         else   
-            #you need a flash error message to tell you if something went wrong
-            flash[:error] = "Please try again. Username or Password was not correct"
-            redirect_to login_path
+            flash[:error] = "Please try again. Username or Password was not correct" #tell the user if login failed
+            redirect_to login_path #redirect the user to the login page
         end 
     end 
 end
