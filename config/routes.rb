e13a0d2142f6e3baec69032c4db7b67d2442573d
Hannah Malm <hannahemalm616@gmail.com>
2021-10-12 21:54:49 -0400

    updated sessions methods and route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
 #add in session and index routes
   get '/' => 'sessions#index'
-  get '/login' => 'session#new' #this starts a new session when a user logs in 
+  get '/login' => 'sessions#new' #this starts a new session when a user logs in 
   post '/login' => 'sessions#create' #post the login information to create the session
   get '/signup' => 'users#new'
   #you do not need a post sign up here because the user will be created 
