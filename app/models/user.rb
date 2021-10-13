361998ec484cc7e998e030dc7a2008278232a9f3
Hannah Malm <hannahemalm616@gmail.com>
2021-10-12 22:00:09 -0400

    add has_secure_password and bcrypt gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
     has_many :ratings
     has_many :rated_pizzas, through: :ratings, source: :pizza
     has_many :pizzas #pizzas that the user creates within the application
+
+    has_secure_password
 end
