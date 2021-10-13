f45363a89a13b265d808b8860658760b941c5dc8
Hannah Malm <hannahemalm616@gmail.com>
2021-10-13 19:54:32 -0400

    added in create and pizza params in the pizzas controller

--- app/controllers/pizzas_controller.rb
+++ app/controllers/pizzas_controller.rb
@@ -1,6 +1,21 @@
 class PizzasController < ApplicationController
 
     def new 
+        @pizza = Pizza.new 
+        @pizza.build_restaurant
     end 
-    
+
+    def create 
+        @pizza = Pizza.new(pizza_params)
+        @pizza.user_id = session[:user_id]
+        if @pizza.save 
+            redirect_to pizza_path(@pizza)
+        else 
+            render :new 
+        end 
+    end 
+
+    def pizza_params
+        params.require(:pizza).permit(:specialtyname, :toppings, :restaurant_id, :restaurant_attributes [:name])
+    end 
 end
