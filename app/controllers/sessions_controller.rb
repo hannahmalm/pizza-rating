3a932826660e292cbfab0030d4038c500964c0b0
Hannah Malm <hannahemalm616@gmail.com>
2021-10-13 18:08:07 -0400

    add in authentication within the user login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,15 +16,19 @@ class SessionsController < ApplicationController
 
     def create 
         @user = User.find_by(username: params[:user][:username])
+            #try is an active support method, calling try on the user, if the user is not nil, then they are authenticating the params password against what the user put in
+            #before calling the method, it trys to see if the user is nil
+        if @user.try(:authenticate, params[:user][:password])
             session[:user_id] = @user.id 
             redirect_to user_path(@user)
         # if @user && @user.authenticate(password: params[:user][:password])
         #     session[:user_id] = @user.id 
         #     #user_path(@user) is the same as user_path(user_id)
         #     redirect_to user_path(@user)
-        # else   
-        #     redirect_to '/'
-        #     #redirect_to login_path
-        # end 
+        else   
+            #you need a flash error message to tell you if something went wrong
+            flash[:error] = "Please try again. Username or Password was not correct"
+            redirect_to login_path
+        end 
     end 
 end
--- notes.md
+++ notes.md
@@ -1,7 +1,7 @@
 1. Create Models & Migrate - rails g resource User username:string password_digest:string 
     - repeat this until all models are created and run rake db migrate
 2. Add in any basic validations, belongs_to, has_many and has_secure_password in User 
-    - ensure you enable bcrypt app 
+    - ensure you enable bcrypt app - bcrypt will hash and salt the password so we cant see this in my database
 3. Add in session controller 
     - The session is responsible for logging in/out a user
 4. Add in session(Login/Logout) routes 
@@ -15,6 +15,12 @@
     -  **IT IS THE JOB OF THE USER TO SIGN UP, NOT THE SESSION ****
         get '/signup' => 'users#new'
 5. Add in methods within session controller
+    index - 
+    destroy
+    new
+    create - correlates to the login
+        - Here we try to find the user in the system
+        - Did we find someone and did they put in the correct password?
 
 6. Add in Views within the session views
 
