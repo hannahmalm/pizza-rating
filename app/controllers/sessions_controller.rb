37fceb551aae8b61de831fbab2134034f602f4a5
Hannah Malm <hannahemalm616@gmail.com>
2021-10-13 17:08:58 -0400

    added create method in sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,14 +2,27 @@ class SessionsController < ApplicationController
     def index 
     end 
 
-    def new 
-        @user = User.new
-    end 
+  
 
     def destroy 
         #the logout link/button should be within the layouts page
         #in rails use delete instead of destroy - destroy is the preferred command when deleting objects, not sessions
         session.delete(:user_id)
         redirect to '/'
+    end 
+
+    def new 
+    
+    end 
+
+    def create 
+        @user = User.find_by(username: params[:user][:username])
+        if @user && @user.authenticate(password: params[:user][:password])
+            session[:user_id] = @user.id 
+            #user_path(@user) is the same as user_path(user_id)
+            redirect_to user_path(@user)
+        else   
+            redirect_to login_path
+        end 
     end 
 end
