bcce930f82cdec13fe9cdbf9fe53cf107ac838ab
Hannah Malm <hannahemalm616@gmail.com>
2021-10-13 19:01:46 -0400

    added a collection select for resturants that belong to a pizza

--- app/models/pizza.rb
+++ app/models/pizza.rb
@@ -3,4 +3,14 @@ class Pizza < ApplicationRecord
   belongs_to :restaurant
   has_many :ratings
   has_many :users, through: :ratings #these are the users who have rated the pizza
+
+  #the belongs_to resttaurant gives us a reader method for restaurant 
+    #def resturant=
+    #end 
+
+    #def build_restuants 
+    #end 
+
+    #def resturant_id 
+    #end 
 end
--- notes.md
+++ notes.md
@@ -1,5 +1,5 @@
 ***Update Readme***
-
+Resources - apidoc.com
 1. Create Models & Migrate - rails g resource User username:string password_digest:string 
     - repeat this until all models are created and run rake db migrate
 2. Add in any basic validations, belongs_to, has_many and has_secure_password in User 
