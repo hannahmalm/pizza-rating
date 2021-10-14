05290aca0bf48d9acbed7dea4111b86975a1e199
Hannah Malm <hannahemalm616@gmail.com>
2021-10-14 18:51:15 -0400

    added new method in rating

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,6 +1,9 @@
 class RatingsController < ApplicationController
 
     def new 
+        #route will look like /pizzas/1/ratings/new
+        @pizza = Pizza.find_by_id(params[:id])
+        @rating = @pizza.build_rating 
     end 
 
 
