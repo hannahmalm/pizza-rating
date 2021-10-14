ac253b79db3d7abd94c55f258e776a4bb3a893dd
Hannah Malm <hannahemalm616@gmail.com>
2021-10-14 19:47:35 -0400

    adjusted rate form for pizza

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -2,10 +2,10 @@ class RatingsController < ApplicationController
 
     def new 
         #route will look like /pizzas/1/ratings/new
-        if @pizza = Pizza.find_by_id(params[:id])
+        if @pizza = Pizza.find_by_id(params[:id]) #if the pizza is created
             @rating = @pizza.rate.build  #rating belongs to a pizza
         else 
-            @rating = Rating.new
+            @rating = Rating.new #create a new rating
         end 
     end 
 
