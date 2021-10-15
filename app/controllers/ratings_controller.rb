d3233bdf7b41f233d4859335689b676cf39e68a4
Hannah Malm <hannahemalm616@gmail.com>
2021-10-14 20:03:38 -0400

    reconfigured new rating form

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -2,8 +2,8 @@ class RatingsController < ApplicationController
 
     def new 
         #route will look like /pizzas/1/ratings/new
-        if @pizza = Pizza.find_by_id(params[:id]) #if the pizza is created
-            @rating = @pizza.rate.build  #rating belongs to a pizza
+        if @pizza = Pizza.find_by_id(params[:pizza_id]) #if the pizza is created
+            @rating = @pizza.ratings.build  #rating belongs to a pizza
         else 
             @rating = Rating.new #create a new rating
         end 
