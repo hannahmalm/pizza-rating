fae374bc436a75d5e22978e5f69433e9718dacbf
Hannah Malm <hannahemalm616@gmail.com>
2021-10-14 20:31:27 -0400

    updated rating model validations

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -3,7 +3,8 @@ class Rating < ApplicationRecord
   belongs_to :pizza
 
   validates :title, presence: true
-  validates :rate, presence: true
+  validates :rate, presence: true, 
+  validates :rate, presence: true, :numericality => { greater_than_or_equal_to: 0, less_than: 11 }
   validates :comment, presence: true
   
 end
