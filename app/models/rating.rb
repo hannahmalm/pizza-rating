0d80e92efecbb508b5cde37975350c005118ef12
Hannah Malm <hannahemalm616@gmail.com>
2021-10-15 14:18:47 -0400

    added scope error for rating a pizza

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -5,5 +5,8 @@ class Rating < ApplicationRecord
   validates :title, presence: true
   validates :rate, numericality: { greater_than: 0}
   validates :comment, presence: true
+
+  #validate that if the pizza id has already been added, uniqueness scope is on the user id
+  validates :pizza, uniqueness: {scope: :user, message: 'You have already rated this' }
   
 end
