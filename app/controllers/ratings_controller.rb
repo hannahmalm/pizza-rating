7c005e8d04f566ea89eb28eb3d4fabd2f77a8299
Hannah Malm <hannahemalm616@gmail.com>
2021-10-17 14:45:55 -0400

    deleted edit and update path for rating

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -35,15 +35,6 @@ class RatingsController < ApplicationController
         end 
     end 
 
-    def edit 
-        @rating = Rating.find(params[:id])
-            if @rating&& @rating.user == current_user
-                redirect_to edit_rating_path(@rating)
-            else 
-                redirect_to rating_path(@rating)
-            end 
-        end 
-
     private 
     #because we are doing a hidden field, you need to specify this 
         def rating_params 
