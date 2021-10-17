5444db9dffa5e61f9ce8a7939ca64d85f96ed2bb
Hannah Malm <hannahemalm616@gmail.com>
2021-10-17 14:26:16 -0400

    updated edit logic

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -36,16 +36,12 @@ class RatingsController < ApplicationController
     end 
 
     def edit 
-        #if logged_in?
         @rating = Rating.find(params[:id])
             if @rating&& @rating.user == current_user
                 redirect_to edit_rating_path(@rating)
             else 
                 redirect_to rating_path(@rating)
             end 
-            # else 
-            #     redirect to '/login'
-            # end 
         end 
 
     private 
