f33bf6d11400bcfc6a06576e1e726e19fe18608f
Hannah Malm <hannahemalm616@gmail.com>
2021-10-16 20:01:29 -0400

    added in links to application layout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,6 @@ class UsersController < ApplicationController
 
     def show 
         @user = User.find_by_id(params[:id])
-        #@user = User.find_by(params[:username])
     end 
 
 
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,8 +1,9 @@
 Rails.application.config.middleware.use OmniAuth::Builder do 
+    provider :github, ENV["GITHUB_CLIENT_ID"], ENV["GITHUB_CLIENT_SECRET"]
     OmniAuth.config.allowed_request_methods = [:post, :get]
-    # OmniAuth.config.silence_get_warning = true
+    OmniAuth.config.silence_get_warning = true
     provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"], ski_jwt: 
     true 
 
-    provider :github, ENV["GITHUB_CLIENT_ID"], ENV["GITHUB_CLIENT_SECRET"]
+    
 end 
