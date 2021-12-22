0b0124ffd31bef3bf4315a59d1a75ff136292a13
Hannah Malm <hannahemalm616@gmail.com>
2021-12-22 17:46:34 -0500

    updated helper method for current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,13 @@
 class ApplicationController < ActionController::Base
 
+    helper_method :current_user
+
     private 
 
     def current_user #this method needs to return user that is found by session user id
         #||= will only call db once if you are using current user more than once in a method 
         #find by id so that if the id is null it wont error out
-        @current_user ||= User.find_by_id(session[:user_id])
+        @current_user ||= User.find_by_id(session[:user_id]) 
     end 
 
 end
